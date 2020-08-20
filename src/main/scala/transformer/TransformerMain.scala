0682aa2e34f9b9b2414ba214c93f5340975bf50d
Justin Deters <justindeters@gmail.com>
2020-08-20 10:29:23 -0500

    added other quasiquotes for completeness for statements

--- src/main/scala/transformer/TransformerMain.scala
+++ src/main/scala/transformer/TransformerMain.scala
@@ -1,65 +1,19 @@
 package transformer
 
-import chisel3._
-import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
-import chisel3.aop.injecting.InjectingAspect
 import scala.meta._
 import scala.meta.contrib._
 
 
 object TransformerMain extends App {
-
-  def around(oldCode: Tree, newCode: Tree) = new Transformer {
-    override def apply(tree: Tree): Tree = {
-      if (tree.isEqual(oldCode)) {
-        newCode
-      } else {
-        super.apply(tree)
-      }
-    }
-  }
-
-  def before(oldCode: Stat, newCode: Stat) = new Transformer {
-    override def apply(tree: Tree): Tree = {
-      tree match {
-        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }" => {
-          val newBodyStats = bodyStats.flatMap(stat =>
-            if (stat.isEqual(oldCode))
-              Seq(newCode, oldCode)
-            else Seq(stat)
-          )
-          template"{ ..$stats } with ..$inits { $self => ..$newBodyStats }"
-        }
-        case _ => super.apply(tree)
-      }
-    }
-  }
-
-  def after(oldCode: Stat, newCode: Stat) = new Transformer {
-    override def apply(tree: Tree): Tree = {
-      tree match {
-        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }" => {
-          val newBodyStats = bodyStats.flatMap(stat =>
-            if (stat.isEqual(oldCode))
-              Seq(oldCode, newCode)
-            else Seq(stat)
-          )
-          template"{ ..$stats } with ..$inits { $self => ..$newBodyStats }"
-        }
-        case _ => super.apply(tree)
-      }
-    }
-  }
-
   val path = java.nio.file.Paths.get("/home/whytheam/Research/chisel/riscv-mini/src/main/scala", "Datapath.scala")
   val bytes = java.nio.file.Files.readAllBytes(path)
   val text = new String(bytes, "UTF-8")
   val input = scala.meta.inputs.Input.VirtualFile(path.toString, text)
   val sourceTree = input.parse[Source].get
 
-  val insertModuleTree = before(q"val regFile = Module(new RegFile)", q"val instCounts = Module(new InstructionCounters)")(sourceTree)
+  val insertModuleTree = Advice.before(q"val regFile = Module(new RegFile)", q"val instCounts = Module(new InstructionCounters)")(sourceTree)
 
-  val finalTree = after(q"io.dcache.abort := csr.io.expt", q"instCounts.io.inst := ew_inst")(insertModuleTree)
+  val finalTree = Advice.after(q"io.dcache.abort := csr.io.expt", q"instCounts.io.inst := ew_inst")(insertModuleTree)
 
   println(finalTree.syntax)
 }
---
+++ src/main/scala/transformer/advice.scala
@@ -0,0 +1,87 @@
+package transformer
+
+import scala.meta._
+import scala.meta.contrib._
+
+object Advice {
+
+  def around(oldCode: Stat, newCode: Stat) = new Transformer {
+    override def apply(tree: Tree): Tree = {
+      if (tree.isEqual(oldCode)) {
+        newCode
+      } else {
+        super.apply(tree)
+      }
+    }
+  }
+
+  def before(oldCode: Stat, newCode: Stat) = new Transformer {
+    def insertBefore(bodyStats: List[Stat]): List[Stat] = bodyStats.flatMap(stat =>
+      if (stat.isEqual(oldCode))
+        Seq(newCode, oldCode)
+      else Seq(stat)
+    )
+
+    override def apply(tree: Tree): Tree = {
+      tree match {
+        //note: ${insertBefore(bodyStats)} is a function call inside the quasiquote
+        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }"
+          if bodyStats.exists(_.isEqual(oldCode)) =>
+            template"{ ..$stats } with ..$inits { $self => ..${insertBefore(bodyStats)} }"
+
+        case q"{ ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"{ ..${insertBefore(stats)} }"
+
+        case q"new { ..$stat } with ..$inits { $self => ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"new { ..$stat } with ..$inits { $self => ..${insertBefore(stats)}}"
+
+        case q"package $eref { ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"package $eref { ..${insertBefore(stats)} }"
+
+        case source"..$stats"
+          if stats.exists(_.isEqual(oldCode)) =>
+            source"..${insertBefore(stats)}"
+
+        case _ => super.apply(tree)
+      }
+    }
+  }
+
+  def after(oldCode: Stat, newCode: Stat) = new Transformer {
+    def insertAfter(bodyStats: List[Stat]): List[Stat] = bodyStats.flatMap(stat =>
+      if (stat.isEqual(oldCode))
+        Seq(oldCode, newCode)
+      else Seq(stat)
+    )
+
+    override def apply(tree: Tree): Tree = {
+      tree match {
+        //note: ${insertBefore(bodyStats)} is a function call inside the quasiquote
+        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }"
+          if bodyStats.exists(_.isEqual(oldCode)) =>
+            template"{ ..$stats } with ..$inits { $self => ..${insertAfter(bodyStats)} }"
+
+        case q"{ ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"{ ..${insertAfter(stats)} }"
+
+        case q"new { ..$stat } with ..$inits { $self => ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"new { ..$stat } with ..$inits { $self => ..${insertAfter(stats)}}"
+
+        case q"package $eref { ..$stats }"
+          if stats.exists(_.isEqual(oldCode)) =>
+            q"package $eref { ..${insertAfter(stats)} }"
+
+        case source"..$stats"
+          if stats.exists(_.isEqual(oldCode)) =>
+            source"..${insertAfter(stats)}"
+
+        case _ => super.apply(tree)
+      }
+    }
+  }
+}
