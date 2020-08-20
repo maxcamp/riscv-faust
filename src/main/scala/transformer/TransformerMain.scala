50416b5a3bba6ce9197e6ef5e3762b743730562e
Justin Deters <justindeters@gmail.com>
2020-08-20 09:35:25 -0500

    Implimented basic advice for statements, hardcoded transforms for risc-v-mini

--- src/main/scala/transformer/TransformerMain.scala
+++ src/main/scala/transformer/TransformerMain.scala
@@ -4,15 +4,62 @@ import chisel3._
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 import chisel3.aop.injecting.InjectingAspect
 import scala.meta._
+import scala.meta.contrib._
+
 
 object TransformerMain extends App {
-  val path = java.nio.file.Paths.get("/home/whytheam/Research/chisel/Adder/src/main/scala/adder", "Adder.scala")
+
+  def around(oldCode: Tree, newCode: Tree) = new Transformer {
+    override def apply(tree: Tree): Tree = {
+      if (tree.isEqual(oldCode)) {
+        newCode
+      } else {
+        super.apply(tree)
+      }
+    }
+  }
+
+  def before(oldCode: Stat, newCode: Stat) = new Transformer {
+    override def apply(tree: Tree): Tree = {
+      tree match {
+        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }" => {
+          val newBodyStats = bodyStats.flatMap(stat =>
+            if (stat.isEqual(oldCode))
+              Seq(newCode, oldCode)
+            else Seq(stat)
+          )
+          template"{ ..$stats } with ..$inits { $self => ..$newBodyStats }"
+        }
+        case _ => super.apply(tree)
+      }
+    }
+  }
+
+  def after(oldCode: Stat, newCode: Stat) = new Transformer {
+    override def apply(tree: Tree): Tree = {
+      tree match {
+        case template"{ ..$stats } with ..$inits { $self => ..$bodyStats }" => {
+          val newBodyStats = bodyStats.flatMap(stat =>
+            if (stat.isEqual(oldCode))
+              Seq(oldCode, newCode)
+            else Seq(stat)
+          )
+          template"{ ..$stats } with ..$inits { $self => ..$newBodyStats }"
+        }
+        case _ => super.apply(tree)
+      }
+    }
+  }
+
+  val path = java.nio.file.Paths.get("/home/whytheam/Research/chisel/riscv-mini/src/main/scala", "Datapath.scala")
   val bytes = java.nio.file.Files.readAllBytes(path)
   val text = new String(bytes, "UTF-8")
   val input = scala.meta.inputs.Input.VirtualFile(path.toString, text)
-  val exampleTree = input.parse[Source].get
-  val newText = exampleTree.transform {
-    case q"new OneBitAdder()" => q"new OneBitAdder() with CarryLookaheadIO"
-  }.toString
-  println(newText)
+  val sourceTree = input.parse[Source].get
+
+  val insertModuleTree = before(q"val regFile = Module(new RegFile)", q"val instCounts = Module(new InstructionCounters)")(sourceTree)
+
+  val finalTree = after(q"io.dcache.abort := csr.io.expt", q"instCounts.io.inst := ew_inst")(insertModuleTree)
+
+  println(finalTree.syntax)
 }
