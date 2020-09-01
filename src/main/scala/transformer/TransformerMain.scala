4d3ea65a3fe83c56c31edf5f2d2de2395b445629
Justin Deters <justindeters@gmail.com>
2020-09-01 11:58:41 -0500

    Refactored to make advice definition more general and advice gets appled to a directory of files

--- src/main/scala/transformer/TransformerMain.scala
+++ src/main/scala/transformer/TransformerMain.scala
@@ -2,24 +2,61 @@ package transformer
 
 import scala.meta._
 import scala.meta.contrib._
+import java.io.File
+import java.io._
 
 class InstructionCounterAspect (tree: Tree) extends Aspect (tree){
 
   val moduleCreationJoinpoint = q"val regFile = Module(new RegFile)"
   val ioConnectionJoinpoint = q"io.dcache.abort := csr.io.expt"
 
-  addAdvice(Advice.before(moduleCreationJoinpoint, q"val instCounts = Module(new InstructionCounters)"))
+  before(moduleCreationJoinpoint , q"val instCounts = Module(new InstructionCounters)")
 
-  addAdvice(Advice.after(ioConnectionJoinpoint, q"instCounts.io.inst := ew_inst"))
+  after(ioConnectionJoinpoint, q"instCounts.io.inst := ew_inst")
 }
 
 
 object TransformerMain extends App {
-  val path = java.nio.file.Paths.get("/home/whytheam/Research/chisel/riscv-mini/src/main/scala", "Datapath.scala")
-  val bytes = java.nio.file.Files.readAllBytes(path)
-  val text = new String(bytes, "UTF-8")
-  val input = scala.meta.inputs.Input.VirtualFile(path.toString, text)
-  implicit val tree = input.parse[Source].get
 
-  println(new InstructionCounterAspect(tree)().syntax)
+  def getRecursiveListOfFiles(dir: File): Array[File] = {
+    val these = dir.listFiles
+    these ++ these.filter(_.isDirectory).flatMap(getRecursiveListOfFiles)
+  }
+
+  val dir = "/home/whytheam/Research/chisel/riscv-mini/src/main/scala"
+
+  //create the output directory
+  val outputDirName = System.getProperty("user.dir") + "/output"
+  val outputDir = new File(outputDirName)
+  outputDir.mkdir
+
+  //get all the files in the project we want to transform
+  val files = getRecursiveListOfFiles(new File(dir))
+
+  //filter out all the non-scala files
+  val scalaFiles = files.filter(file =>
+    file.getName match {
+      case fileName: String if(fileName.endsWith(".scala")) => true
+      case _ => false
+    }
+  )
+
+  //apply the transformation to every file we've found
+  scalaFiles.foreach(file => {
+    println("Processing " + file.getName)
+    val path = file.toPath
+    val bytes = java.nio.file.Files.readAllBytes(path)
+    val text = new String(bytes, "UTF-8")
+    val input = scala.meta.inputs.Input.VirtualFile(path.toString, text)
+    implicit val tree = input.parse[Source].get
+    val newTree = new InstructionCounterAspect(tree)()
+    if (!newTree.isEqual(tree)) {
+      val newFile = new File(outputDirName + "/" + file.getName)
+      val bw = new BufferedWriter(new FileWriter(newFile))
+      //TODO: Don't hard code this
+      bw.write(new InstructionCounterAspect(tree)().syntax)
+      bw.close()
+      println("Transform Applied in " + file.getName)
+    }
+  })
 }
--- src/main/scala/transformer/aspect.scala
+++ src/main/scala/transformer/aspect.scala
@@ -5,15 +5,24 @@ import scala.meta._
 abstract class Aspect (tree: Tree){
   var adviceSequence = Seq[Transformer]()
 
-  protected def addAdvice(advice: Transformer) {
+  private def addAdvice(advice: Transformer) {
     adviceSequence = adviceSequence :+ advice
   }
 
-  def apply(): Tree = {
-    adviceSequence.foldLeft(tree){
-      (newTree, transform) => {
-        transform(newTree)
-      }
-    }
+  protected def around (oldCode: Stat, newCode: Stat) = {
+    addAdvice(Advice.around(oldCode, newCode))
   }
+
+  protected def before (oldCode: Stat, newCode: Stat) = {
+    addAdvice(Advice.before(oldCode, newCode))
+  }
+
+  protected def after (oldCode: Stat, newCode: Stat) = {
+    addAdvice(Advice.after(oldCode, newCode))
+  }
+
+  def apply(): Tree = {
+    adviceSequence.foldLeft(tree){ (newTree, transform) => { transform(newTree) } }
+  }
+
 }
